8bfe456857bded6c8fd3d5dece814952ee4c66e2
Stefano Agnelli <stefano.agnelli.jobs@gmail.com>
2025-02-01 16:15:55 +0100

    001-sorted-order

---
+++ main.py
@@ -0,0 +1,12 @@
+group_integer = []
+
+user_input = int(input('insert an integer: '))
+
+while user_input != 0:
+    group_integer.append(user_input)
+    user_input = int(input('insert an integer: '))
+
+group_integer.sort()
+
+for numbers in group_integer:
+    print(numbers)
